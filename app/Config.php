a568a62f81c4e439d42be1e295d40a060bb4d601
Shamalka_M <89902167+Shamalka111@users.noreply.github.com>
2022-11-21 22:41:58 +0530

    move new db

--- app/Config.php
+++ app/Config.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_NAME', 'bodo');
+define('DB_NAME', 'bodo1');
 define('DB_PASSWORD', '');
 define("BASEURL", "http://localhost/Bodo");
 
