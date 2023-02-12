80d6aa110fcd18b4aade3f04e37c2a1e6e5096a2
Janindu Indusara <indusarajanindu@gmail.com>
2023-02-12 23:03:58 +0530

    m

--- app/views/property/boardingView.php
+++ app/views/property/boardingView.php
@@ -252,52 +252,52 @@ if (isset($boardingPlace)) {
                         <?php
                         if (!is_null($reviews)) {
                             foreach ($reviews as $res) {    
-                                    echo "
-                                    <div class='row padding-2 shadow margin-2 border-rounded'>
-                                        <div class='row>
-                                            <div class='col-3'>
-                                                <img class=' dp  border-circle' src='https://ui-avatars.com/api/?background=288684&color=fff&name=$res->FirstName+$res->LastName' >
-                                            </div>
-                                            <div class='col-3'>
-                                               <div class='header-nb right'> $res->FirstName $res->LastName</div>
-                                               $res->UserType
-                                            </div>
-                                            <div class='col-2'></div>
-                                            <div class='col-4 left-flex'>";
+                                    // echo "
+                                    // <div class='row padding-2 shadow margin-2 border-rounded'>
+                                    //     <div class='row>
+                                    //         <div class='col-3'>
+                                    //             <img class=' dp  border-circle' src='https://ui-avatars.com/api/?background=288684&color=fff&name=$res->FirstName+$res->LastName' >
+                                    //         </div>
+                                    //         <div class='col-3'>
+                                    //            <div class='header-nb right'> $res->FirstName $res->LastName</div>
+                                    //            $res->UserType
+                                    //         </div>
+                                    //         <div class='col-2'></div>
+                                    //         <div class='col-4 left-flex'>";
 
-                                            if($res->Rating > 4.5){
-                                                echo "
-                                                <img height=20px class=' fill-container' src='$base/public/images/5.jpg' >
-                                                ";
-                                            }else if($res->Rating > 3.5){
-                                                echo "
-                                                <img height=20px class=' fill-container' src='$base/public/images/4.jpg' >
-                                                ";
-                                            }else if($res->Rating > 2.5){
-                                                echo "
-                                                <img height=20px class=' fill-container' src='$base/public/images/3.jpg' >
-                                                ";
-                                            }else if($res->Rating > 1.5){
-                                                echo "
-                                                <img height=20px class=' fill-container' src='$base/public/images/2.jpg' >
-                                                ";
-                                            }else{
-                                                echo "
-                                                <img height=20px class=' fill-container' src='$base/public/images/1.jpg' >
-                                                ";
-                                            }
+                                    //         if($res->Rating > 4.5){
+                                    //             echo "
+                                    //             <img height=20px class=' fill-container' src='$base/public/images/5.jpg' >
+                                    //             ";
+                                    //         }else if($res->Rating > 3.5){
+                                    //             echo "
+                                    //             <img height=20px class=' fill-container' src='$base/public/images/4.jpg' >
+                                    //             ";
+                                    //         }else if($res->Rating > 2.5){
+                                    //             echo "
+                                    //             <img height=20px class=' fill-container' src='$base/public/images/3.jpg' >
+                                    //             ";
+                                    //         }else if($res->Rating > 1.5){
+                                    //             echo "
+                                    //             <img height=20px class=' fill-container' src='$base/public/images/2.jpg' >
+                                    //             ";
+                                    //         }else{
+                                    //             echo "
+                                    //             <img height=20px class=' fill-container' src='$base/public/images/1.jpg' >
+                                    //             ";
+                                    //         }
                                                 
-                                        echo"
-                                            </div>
-                                            <div class='row'>
-                                                <div class='col-12'>
-                                                $res->Review
-                                                </div>
-                                            </div>
-                                        </div>
+                                    //     echo"
+                                    //         </div>
+                                    //         <div class='row'>
+                                    //             <div class='col-12'>
+                                    //             $res->Review
+                                    //             </div>
+                                    //         </div>
+                                    //     </div>
                                         
                                             
-                                    ";     
+                                    // ";     
                             }
                         } else {
                         
