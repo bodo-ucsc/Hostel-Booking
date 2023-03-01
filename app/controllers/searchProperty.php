b77e971b9c16d675010c37420cf662b633b578c5
Shamalka_M <89902167+Shamalka111@users.noreply.github.com>
2023-03-01 21:53:00 +0530

    Update searchProperty.php

--- app/controllers/searchProperty.php
+++ app/controllers/searchProperty.php
@@ -13,36 +13,14 @@ class SearchProperty extends Controller
             $search_query = $_POST['q'];
 
             $result=$this->model('viewModel')->searchBoarding($search_query);
-            $this->view('search', ['result' => $result]);
+      
+            $this->view('search', ['result' => $result, 'searchText' => $search_query]);
             // Pass the search results to a view for display
             //$this->view->render('search_results', $results);
 
         }else{
             echo "Not submitted";
         }
-        // echo $info;
-        // if (isset($info)) {
-
-        //     $info = explode(" ", $info);
-        //     $info = implode("%", $info);
-        //     $info = "%" . $info . "%";
-        //     echo $info;
-            //$this->view("search", ["info" => $info]);
-            //$result = $this->model('viewModel')->searchPlace($info);
-            //$this->view('search/searchPlace', ['result' => $result]);
-    
-    
-
- 
-    
-
-        // $res = $this->model('viewModel')->checkUser('Username', $username);
-        // $res = $res->fetch_assoc();
-        // if ($res != null) {
-
-        //     $message = "User name already exists";
-        //     header("Location: " . BASEURL . "/admin/userManagement/boardingOwner/1/2/$message");
-        // }
     }
 }
 
