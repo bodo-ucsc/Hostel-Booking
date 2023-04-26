b11ba8c2cbad40f7d7ab55135eb02493ec202886
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-04-26 20:21:11 +0530

    Update about us

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -108,9 +108,9 @@ $header = new HTMLHeader("About");
              -->
             <div class="col-5">
                 <!-- <div class="col-12 col-medium-5 width-90"> -->
-    
 
-            
+
+
                 <div class="row">
                     <div class="display-none display-medium-block col-medium-12 padding-bottom-3">
                         <img class="fill-container" style="height: 400px; width: 400px;"
@@ -134,7 +134,11 @@ $header = new HTMLHeader("About");
 
                 </div>
                 <div class="row">
-                    <div class="col-8 text-5 fill-container">A website dedicated to providing easy and convenient access to hostels and boarding houses for students and professionals. Our team consists of second-year undergraduate students from the University of Colombo School of Computing who are passionate about creating innovative solutions that can benefit our fellow students and the wider community.</div>
+                    <div class="col-8 text-5 fill-container">A website dedicated to providing easy and convenient access
+                        to hostels and boarding houses for students and professionals. Our team consists of second-year
+                        undergraduate students from the University of Colombo School of Computing who are passionate
+                        about creating innovative solutions that can benefit our fellow students and the wider
+                        community.</div>
                 </div>
 
 
@@ -154,6 +158,8 @@ $header = new HTMLHeader("About");
 
 
             </div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
 
 
 
@@ -164,7 +170,239 @@ $header = new HTMLHeader("About");
 
 
 
+
+
     </div>
+
+    <div class="fill-container">
+        <div class="row bg-black">
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+
+            <div class="col-4"></div>
+            <div class="col-4">
+                <div class="title-1">Our Services</div>
+            </div>
+            <div class="col-4"></div>
+            <div class="col-12"></div>
+
+        </div>
+
+
+        <!-- <div class="row bg-black fill-container">
+
+            <div class="padding-3 bg-white border-rounded  fill-container col-12  display-medium-block">
+                <div class="row bg-black  margin-top-2 fill-container">
+                    <div class="col-1"></div>
+                    <div class='col-3 padding-3 margin-2'>
+                        <div class="shadow-small border-rounded-more accent padding-4 bg-accent">
+                            <div class='text-4 white'>Hostel Booking</div>
+                            <div class='text-5 white'>BODO can be highly beneficial for users as it saves time, is
+                                cost-effective, provides user reviews and ratings, offers a wide selection of options,
+                                and is convenient for booking and payment. Users can easily find and book affordable
+                                accommodations.</div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+
+
+        </div>/////// -->
+        <div class="row bg-black fill-container">
+            <div class="padding-3 border-rounded  fill-container col-12  display-medium-block">
+                <div class="row margin-top-2 fill-container">
+                    <div class="col-1"></div>
+                    <div class='col-3 padding-3 margin-2'>
+                        <div class="shadow-small padding-4 bg-black1 ">
+                            <div class='text-4 white'>Hostel Booking</div>
+                            <div class='text-5 white'>BODO can be highly beneficial for users as it saves time, is
+                                cost-effective, provides user reviews and ratings, offers a wide selection of options,
+                                and is convenient for booking and payment.</div>
+                        </div>
+                    </div>
+
+                    <div class='col-4 padding-3 margin-1'>
+                        <div class="shadow-small  padding-4 bg-black1">
+                            <div class='text-4 white'>Advertising</div>
+                            <div class='text-5 white'>Advertising boarding places on BODO can provide increased
+                                bookings, cost-effective advertising, positive user reviews, streamlined booking
+                                processes, and access to analytics and data. This can help owners attract more guests,
+                                improve their reputation, and make data-driven decisions about their business
+                                strategies.</div>
+                        </div>
+                    </div>
+
+                    <div class='col-3 padding-3 margin-2'>
+                        <div class="shadow-small  padding-4 bg-black1">
+                            <div class='text-4 white'>Hostel Managing</div>
+                            <div class='text-5 white'>BODO helps borders to manage their bookings, payments, room
+                                allocation, information, and receive guest reviews and feedback more efficiently. This
+                                can help improve their overall efficiency, reduce administrative burden..</div>
+                        </div>
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-12"></div>
+                    <div class="col-12"></div>
+                    <div class="col-12"></div>
+                    <div class="col-12"></div>
+
+
+
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+    <div class="fill-container">
+        <div class="row">
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+
+            <div class="col-4"></div>
+            <div class="col-4">
+                <div class="title">Our Skills</div>
+
+            </div>
+            <div class="col-4"></div>
+            <div class="col-12"></div>
+
+        </div>
+
+
+
+
+
+
+    </div>
+    <div class="row fill-container">
+        <div class="padding-3 border-rounded  fill-container col-12  display-medium-block">
+            <div class="row margin-top-2 fill-container">
+                <div class="col-1"></div>
+                <div class='col-5 padding-3 margin-2'>
+                    <div class="padding-5">
+                        <div class="col-4 text-41 fill-container">Our creative skills & experiences.</div>
+                        <div class='text-5'>As second year undergraduate students of University of Colombo School of
+                            Computing, our web development skills include proficiency in programming languages such as
+                            HTML, CSS, JavaScript, and PHP. We also have experience with web development frameworks such
+                            as React, Angular, and Vue. In addition to technical skills, we possess strong
+                            problem-solving and critical thinking skills, attention to detail, and the ability to work
+                            collaboratively in a team environment. We also have a creative mindset and an understanding
+                            of user experience (UX) and user interface (UI) design principles. These skills enable us to
+                            design and create engaging and effective websites, and we are excited to showcase our skills
+                            through this web site.</div>
+                    </div>
+                </div>
+
+
+
+                <div class='col-5 padding-3 margin-2'>
+
+                    <!-- <div class=" padding-4 ">
+                        
+                    </div> -->
+
+                    <div class="row">
+                        <div class="display-none display-medium-block col-medium-12 padding-bottom-3">
+                            <img class="fill-container" src="<?php echo BASEURL . '/public/images/UI.svg' ?>">
+
+                        </div>
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-12"></div>
+
+
+
+
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+
+
+    <div class="fill-container">
+        <div class="row bg-black">
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+
+            <div class="col-4"></div>
+            <div class="col-4">
+                <div class="title-1">Our Team</div>
+            </div>
+            <div class="col-4"></div>
+            <div class="col-12"></div>
+
+        </div>
+
+
+
+        <div class="row fill-container">
+            <div class="row">
+                <div class="col-12"></div>
+                <div class="col-4">
+                    <div class="image">
+                        <img src="<?php echo BASEURL . '/public/images/1.jpg' ?>">
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
 
 
 
